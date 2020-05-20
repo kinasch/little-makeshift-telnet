7f4e561fd9b0ae8c34a6372918a268a62a8cd592
kinasch <janik@schoenberg.name>
2020-05-20 22:26:43 +0200

    SMTP update2: error(Reader doesn't end)

--- src/ueb1/SMTP.java
+++ src/ueb1/SMTP.java
@@ -4,14 +4,16 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.Timer;
 
 public class SMTP {
+
     public static void main(String[] args){
         mail(args);
     }
     public static void mail(String[] args){
         if (args.length!=2){
-            System.err.println("Usage: java smtp.java <mail address 1> <mail address 2>");
+            System.err.println("Usage:  java SMTP <mail address 1> <mail address 2>");
             System.exit(1);
         }
         try  {
@@ -20,12 +22,19 @@ public class SMTP {
             PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
             BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
 
-            out.println("ehlo smtp.htw-berlin.de");
+            String ans1;
+            ans1 = in.readLine();
+            System.out.println(ans1);
+
+            out.print("ehlo smtp.htw-berlin.de\r\n");
+            out.flush();
             System.out.println("ehlo smtp.htw-berlin.de");
-            String answer;
-            while((answer = in.readLine())!=null){
-                System.out.println(answer);
+            String ans2;
+            while((ans2 = in.readLine())!=null){
+                System.out.println(ans2);
             }
+            out.print("mail from: "+args[0]+"\r\n");
+            System.out.println("mail from: "+args[0]+"\r\n");
 
         } catch (Exception e) {
             System.err.println("Error: "+e);
